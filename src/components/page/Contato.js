6941390be9d69ea99cedbeb4fd4a184b804eef6a
Eduardo <eduardo.m.p.freitas@gmail.com>
2021-12-02 20:56:31 -0300

    Segunda versão do projeto

---
+++ src/components/page/Contato.js
@@ -0,0 +1,6 @@
+function Contato() {
+    
+    return<h1>Pagina Contato</h1>
+}
+
+export default Contato
---
+++ src/components/page/NovoProjeto.js
@@ -0,0 +1,6 @@
+function NovoProjeto() {
+    
+    return<h1>Pagina Novo Projeto</h1>
+}
+
+export default NovoProjeto
